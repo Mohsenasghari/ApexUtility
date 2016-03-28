eb7110999d60da10f1df9ba4527eb25c83787eca
Mohsenasghari <mohsen.asghari@gmail.com>
2016-03-28 23:56:19 +0430

    LatexOutPut

---
+++ Entity/LatexOutPut.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApexUtility.Entity
+{
+    public class OutPut
+    {
+        public static string TableOutPut { get; set; }
+        public static Dictionary<string, List<double>> Results = new Dictionary<string, List<double>>();
+        public static int IndexRun = 0;
+    }
+}
